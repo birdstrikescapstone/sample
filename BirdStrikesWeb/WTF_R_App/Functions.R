7b10575155cd1b880589ad4bc654694e52c91b98
Krishna Prasad <kp2558@stern.nyu.edu>
2020-04-27 12:34:53 -0600

    Added the new functions to get data and predict, modified server to incorporate the models.

--- BirdStrikesWeb/WTF_R_App/Functions.R
+++ BirdStrikesWeb/WTF_R_App/Functions.R
@@ -55,23 +55,89 @@
                   -87.9048,
                   -121.5910034),
     strikes = c(2814,
-                2170,1557,1891))
+                2170, 1557, 1891),
+    modelfilename = c(
+      "model_xgb_KDEN.RDS",
+      "model_rf_KDFW.RDS",
+      "model_xgb_KORD.RDS",
+      "model_xgb_KSMF.RDS"
+    ),
+    avgfilename = c(
+      "Avgs_KDEN.RDS",
+      "Avgs_KDFW.RDS",
+      "Avgs_KORD.RDS",
+      "Avgs_KSMF.RDS"
+    )
+  )
   
-  #Model Information 
-  source("Model.R")
+  # #Model Information 
+  # source("Model.R")
   
-  #getAverageByInput("KDEN",Sys.Date())
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  ########################################################################
+  # Purpose: Function generates the input to the model based on the
+  # Averages of the strikes dataset based in Day Of Year. After generating 
+  # the input, executes the predict on the model and returns the  
+  # strike risk level and probability
+  #
+  # Input: Airfield - KDEN/KDFW/KORD/KSMF
+  #        StrikeDate - Date to display the Strike Risk level
+  #
+  # Output: Dataframe containing data for model Input
+  #######################################################################
+  getDataAndRunPredict <- function(airfield, strikedate) {
+    
+    # Get Averages file name from the custom dataframe
+    fileNames <-
+      air %>% select(avgfilename, modelfilename) %>% filter(airfields == airfield)
+
+    
+    # Read the Averages from file system
+    data <- readRDS(paste0(fileNames$avgfilename))
+
+    # Convert the input to date format and convert to Day of Week
+    data <-
+      data %>% filter(DAYOFYEAR == lubridate::yday(as.Date(strikedate)))
+
+    # Create Season columns as required by the input to the model
+    data <-
+      data %>% add_column(
+        SEASON.winter = 0,
+        SEASON.summer = 0,
+        SEASON.fall = 0,
+        SEASON.spring = 0
+      )
+
+    # Fill the season columns based on the value in the Averages dataframe
+    if (data$SEASON == 1) {
+      data$SEASON.winter <- 1
+    } else if (data$SEASON == 2) {
+      data$SEASON.spring <- 1
+    } else if (data$SEASON == 3) {
+      data$SEASON.summer <- 1
+    } else {
+      data$SEASON.fall <- 1
+    }
+
+    # Remove Season column and make all columns numeric
+    data <-
+      data %>% select(-SEASON) %>% mutate_all(as.numeric)
+
+    
+    # Load model from RDS
+    model <- readRDS(paste0(fileNames$modelfilename))
+    
+    # Call predict with type = raw to get the risk levels
+    predicted.raw <- predict(model, data, type = "raw")
+    
+    # Call Predict with type = prob to get probabilities
+    predicted.prob <- predict(model, data, type = "prob")
+    
+    predicted.prob
+    # Create results dataframe for use in UI
+    strike.results <-
+      data.frame("STRIKERISKLEVEL" = predicted.raw,
+                 "STRIKEPROBABILITY" = predicted.prob)
+    
+    # Return dataframe
+    strike.results
+  }
--- BirdStrikesWeb/WTF_R_App/server.R
+++ BirdStrikesWeb/WTF_R_App/server.R
@@ -1,4 +1,5 @@
-source("Functions.R")
+source("./Functions.R", local = TRUE)
+# source("Model.R")
 
 function(input, output, session) {
   # 
@@ -13,84 +14,93 @@ function(input, output, session) {
   #   })
   
   output$summary <- renderTable({
-    t <-data %>%
-      filter(`AIRPORT ID`== input$airfield) %>%
-      group_by(`AIRPORT ID`) %>%
-      summarise(STRIKES = sum(STRIKECOUNT)) %>%
-      arrange(-STRIKES)
-    t
+    # t <-data %>%
+    #   filter(`AIRPORT ID`== input$airfield) %>%
+    #   group_by(`AIRPORT ID`) %>%
+    #   summarise(STRIKES = sum(STRIKECOUNT)) %>%
+    #   arrange(-STRIKES)
     
-    #getAverageByInput(input$airfield,input$date)
-
+    getDataAndRunPredict(input$airfield,input$date)
   })
   
   output$map <- renderLeaflet({
-    # coord <- air %>% 
+    # coord <- air %>%
     #   filter(airfields == input$airfield)
     # lat<- coord[1,2]
     # long<-coord[1,3]
     #the opening should show the 4 airfields and a heat map of max to min agg strikes
     
-    mymap <- leaflet() %>% 
+    mymap <- leaflet() %>%
       addProviderTiles(providers$OpenStreetMap,
-                       options = providerTileOptions(noWrap = TRUE)) %>% 
-      # addMarkers(lng = air$longitude, lat = air$latitude, popup = names(air)) %>% 
-      addCircleMarkers(air$longitude,air$latitude,
+                       options = providerTileOptions(noWrap = TRUE)) %>%
+      # addMarkers(lng = air$longitude, lat = air$latitude, popup = names(air)) %>%
+      addCircleMarkers(air$longitude, air$latitude,
                        sqrt(air$strikes) * 0.79899) %>%
-      addPopups(air$longitude,air$latitude,
+      addPopups(air$longitude, air$latitude,
                 htmltools::htmlEscape(paste(
                   paste(air$airfields, sep = " "),
                   paste("Strikes:", as.character(air$strikes), sep = " "),
-                  sep = ", ")))
+                  sep = ", "
+                )))
     mymap
   })
+  
   #Header Boxes
-  output$vboxengf<-renderValueBox({
+  output$vboxengf <- renderValueBox({
     valueBox(
       "Engine Failure:",
       subtitle = tags$p("TEXT", style = "font-size: 200%;"),
       icon = icon("earlybirds"),
-      color = "yellow")
-      })
-  output$vboxstrikes <-renderValueBox({
-    t <- data %>% 
-      filter(`AIRPORT ID`== input$airfield) %>% 
-      group_by(`AIRPORT ID`) %>% 
-      summarise(STRIKES = sum(STRIKECOUNT)) %>% 
-      arrange(-STRIKES)  
+      color = "yellow"
+    )
+  })
+  
+  
+  
+  output$vboxstrikes <- renderValueBox({
+    t <- data %>%
+      filter(`AIRPORT ID` == input$airfield) %>%
+      group_by(`AIRPORT ID`) %>%
+      summarise(STRIKES = sum(STRIKECOUNT)) %>%
+      arrange(-STRIKES)
     
     valueBox(
-        "Historical Strikes:",
-        subtitle = tags$p(t[1,2], style = "font-size: 200%;"),
-        icon = icon("feather"),
-        color = "aqua")
+      "Historical Strikes:",
+      subtitle = tags$p(t[1, 2], style = "font-size: 200%;"),
+      icon = icon("feather"),
+      color = "aqua"
+    )
     
   })
-  output$vboxrisk <-renderValueBox({
-    if(input$date == Sys.Date()){
+  
+  
+  output$vboxrisk <- renderValueBox({
+    risk <- getDataAndRunPredict(input$airfield, input$date)
+    
+    if (risk$STRIKERISKLEVEL == "L") {
       valueBox(
         "Risk Level:",
-        subtitle = tags$p("TEXT", style = "font-size: 200%;"),
+        subtitle = tags$p("LOW", style = "font-size: 200%;"),
         icon = icon("plane"),
-        color = "green")
+        color = "green"
+      )
     }
-    else if(input$date > Sys.Date()){
+    else if (risk$STRIKERISKLEVEL == "H") {
       valueBox(
         "Risk Level:",
-        subtitle = tags$p("TEXT", style = "font-size: 200%;"),
+        subtitle = tags$p("HIGH", style = "font-size: 200%;"),
         icon = icon("plane"),
-        color = "red")
+        color = "red"
+      )
     }
-    else valueBox(
-      "Risk Level:",
-      subtitle = tags$p("TEXT", style = "font-size: 200%;"),
-      icon = icon("plane"),
-      color = "teal")
-
+    else
+      valueBox(
+        "Risk Level:",
+        subtitle = tags$p("MEDIUM", style = "font-size: 200%;"),
+        icon = icon("plane"),
+        color = "teal"
+      )
   })
-  
-  
-  
 }
 
 
